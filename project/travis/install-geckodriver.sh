f3b9bb1c2b4dd017874e8e2d7df02c42f47cc067
mdolinin <dmo.builder@gmail.com>
2018-10-05 09:00:28 -0400

    update firefox and geckodriver for travis

--- project/travis/install-geckodriver.sh
+++ project/travis/install-geckodriver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-export GECKODRIVER_VERSION=v0.11.1
+export GECKODRIVER_VERSION=v0.23.0
 curl -L -o geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/${GECKODRIVER_VERSION}/geckodriver-${GECKODRIVER_VERSION}-linux64.tar.gz
 gunzip -c geckodriver.tar.gz | tar xopf -
 chmod +x geckodriver && sudo mv geckodriver /usr/local/bin
