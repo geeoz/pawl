03b92e5a2e5d3f0a30ada6dc4a3040f64c281312
mdolinin <dmo.builder@gmail.com>
2018-10-08 11:45:32 -0400

    fix path in chrome driver setup script for travis

--- project/travis/install-chromedriver.sh
+++ project/travis/install-chromedriver.sh
@@ -3,7 +3,7 @@ set -ex
 
 export CHROME_DRIVER_VERSION=2.42
 curl -L -o chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d ~/
+unzip chromedriver_linux64.zip
 rm chromedriver_linux64.zip
 sudo mv -f chromedriver /usr/local/share/
 sudo chmod +x /usr/local/share/chromedriver
