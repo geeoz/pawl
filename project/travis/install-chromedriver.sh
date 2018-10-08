801b87f54c0a142501cc0070a4b893cea6ea3d19
mdolinin <dmo.builder@gmail.com>
2018-10-08 11:01:01 -0400

    fix chrome driver setup script for travis

--- project/travis/install-chromedriver.sh
+++ project/travis/install-chromedriver.sh
@@ -2,6 +2,9 @@
 set -ex
 
 export CHROME_DRIVER_VERSION=2.42
-curl -L -o chromedriver.zip http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
-gunzip -c chromedriver.zip | tar xopf -
-chmod +x chromedriver && sudo mv chromedriver /usr/local/bin
+curl -L -o chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/share/
+sudo chmod +x /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
